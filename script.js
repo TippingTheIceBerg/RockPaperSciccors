4107acdfc0f70fbba1645364a7d00cbd4d5c6bdf
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-08-24 18:18:37 -0500

    fix game logic, add style to computer choice

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 let playerChoiceOptions = document.getElementsByClassName("playerChoice")
-
+let winnerText = document.querySelector(".winner__text");
 let playerChoice = ""
+let computerChoice = ""
+let computerNumberStyle = document.getElementsByClassName("computerChoice")
+
 
 
 function getComputerChoice(){
-    let randomComputerChoice = Math.floor(Math.random()*3)
-    return randomComputerChoice;
-}
-
-
-function getPlayerChoice(){
-    // Rock = 0; Paper = 1; Scissors = 2
-    playerChoice === "Rock" ? playerChoice = 0: playerChoice === "Paper" ? playerChoice = 1: playerChoice = 2;
-    return playerChoice}
-
-
-function playRound (playerChoice, computerChoice){
-    playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
-
-    // Logic that decides what beats what
-    computerChoice == 0 && playerChoice == 1? console.log("player wins"): computerChoice == 1 && playerChoice == 2? console.log("player wins"): computerChoice == 2 && playerChoice == 0? console.log("player wins"): computerChoice == 1 && playerChoice == 0? console.log("computer wins"): computerChoice == 2 && playerChoice == 1? console.log("computer wins"): computerChoice == 0 && playerChoice == 2?console.log("computer wins"): console.log("it's a tie")  ;
-  
+    computerChoice = Math.floor(Math.random()*3);
+    return computerChoice;
+    
 }
 
 
@@ -32,3 +20,38 @@ for(let i = 0; i < playerChoiceOptions.length;i++){
     })
    
 }
+function getPlayerChoice(){
+    // Rock = 0; Paper = 1; Scissors = 2
+    playerChoice === "Rock" ? playerChoice = 0: playerChoice === "Paper" ? playerChoice = 1: playerChoice = 2;
+    
+    return playerChoice}
+
+
+function playRound (playerNumber, computerNumber, styleComputer){
+    winnerText.style.display = "block"
+    playerNumber = getPlayerChoice();
+    computerNumber = getComputerChoice();
+    styleComputer = setComputerStyle();
+    // Logic that decides what beats what
+    // Rock = 0; Paper = 1; Scissors = 2
+    playerNumber == 0 && computerNumber == 2? winnerText.innerText = "player Wins": playerNumber == 1 && computerNumber == 0? winnerText.innerText = "player wins": playerNumber == 2 && computerNumber == 1? winnerText.innerText ="player wins": playerNumber == computerNumber? winnerText.innerText = "it's a tie": winnerText.innerText = "computer wins"
+;
+  
+}
+function setComputerStyle(){
+
+    for(let i = 0; i < computerNumberStyle.length; i++ ){
+        computerNumberStyle[i].style.backgroundColor = "rgb(0 0 0/.0)"
+    }   
+    console.log(computerChoice)
+    computerNumberStyle[computerChoice].style.backgroundColor = "red"
+}
+
+function setBackgroundColorOfChoice () {
+    for(let i = 0; i < playerChoiceOptions.length;i++){
+    playerChoiceOptions[i].style.backgroundColor = "red"
+    }}
+
+
+
+
